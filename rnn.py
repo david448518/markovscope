2f28f024ac48ed817cb7053e713a8031d9b99c00
David <david@comman.nl>
2022-01-09 17:10:50 +0100

    Remove unused code

--- rnn.py
+++ rnn.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib import learn
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -19,33 +18,6 @@ import os
 weight_file = sys.argv[-1]
 # specify sequence length
 seq_length = 4
-
-
-def sample(a, temp=1.0):
-    a = np.log(a) / temp
-    a = np.exp(a) / np.sum(np.exp(a))
-    return np.argmax(np.random.multinomial(1, a, 1))
-
-
-def get_seed(seqs):
-    return seqs[random.randint(0, len(seqs))]
-
-
-def data_gen(batch_size):
-    X = np.zeros((batch_size, seq_length-1, len(words)))
-    y = np.zeros((batch_size, len(words)))
-    batch = 0
-    while 1:
-        for i in range(len(sequences)):
-            batchIdx = i % batch_size
-            for j in range(seq_length-1):
-                X[batchIdx, j, sequences[i][j]] = 1
-            y[batchIdx, next_words[i]] = 1
-            if batchIdx == 0 and i != 0:
-                batch += 1
-                print('batch:', batch)
-                yield (X, y)
-
 
 # process the data
 X, y, sequences, next_words, words, word_to_idx, idx_to_word = process(
@@ -74,7 +46,6 @@ else:
 
 
 # generate text using sequences from the data as seeds
-#seed = get_seed(sequences)
 seed = sequences[np.random.randint(0, len(sequences)-1)]
 generated = []
 print('seed: ', seed)
